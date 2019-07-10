6d7ab154d6f2f955782de1c9877ee3a6ea2e9217
Siwik <michalsiwik@gmail.com>
2019-07-10 15:22:28 +0200

    Removed some comments

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 
 func main() {
 
-	//helpFlag := flag.String("h", "", "help, and description of flags")
 	mailsFlag := flag.String("to", "", "mail recipient, if multiple recipients use comma as separator ','")
 	attachmentFlag := flag.String("attach", "", "files you want to attach, if multiple files use comma as separator ','")
 	subjectFlag := flag.String("subj", "Automated mail sended from mailCLI app", "subject of mail message")
@@ -24,13 +23,11 @@ func main() {
 	//msg.SetAddressHeader("Cc", "dan@example.com", "Dan")
 	msg.SetHeader("Subject", *subjectFlag)
 	msg.SetBody("text/html", "Cześć,\nTen mail został wysłany automatycznie za pomocą aplikacji mailCLI w razie jakichkolkwiek pytań, lub problemów proszę o kontakt z <b>michalsiwik@gmail.com</b>!")
-	//msg.Attach("/home/Alex/lolcat.jpg")
 
 	config := readConfig(*configFlag)
 
 	d := gomail.NewDialer(config.Mailserver.SMTPGate, config.Mailserver.SMTPPort, config.Authentication.Login, config.Authentication.Password)
 
-	//fmt.Println(config.Mailserver.FromMail, config.Mailserver.SMTPGate, config.Mailserver.SMTPPort, config.Authentication.Login)
 	if err := d.DialAndSend(msg); err != nil {
 		panic(err)
 	}
