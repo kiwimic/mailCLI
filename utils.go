234986b8b425439864e85e4cdbf977859e96b75d
Siwik <michalsiwik@gmail.com>
2019-07-12 11:20:23 +0200

    #4 Added new version of isBodyFile, and fileExists also added some test in other commit. If this will be fine it will be commited as fix

--- utils.go
+++ utils.go
@@ -17,32 +17,13 @@ func isFlagPassed(name string) bool {
 	return found
 }
 
-func checkConfiguration(conf string, log string) string {
+func fileExists(name string) bool {
 
-	ret := ""
-
-	boolConf, errConf := fileExists(conf)
-	boolLog, errLog := fileExists(log)
-
-	if !boolConf {
-		ret = ret + fmt.Sprintf("config not found at: %s", conf) + "\n"
-		fmt.Println("config file err:", errConf)
+	_, err := os.Open(name)
+	if err != nil {
+		return false
 	}
-
-	if !boolLog {
-		ret = ret + fmt.Sprintf("log file not found at: %s", log)
-		fmt.Println("log file err:", errLog)
-	}
-
-	return ret
-}
-
-func fileExists(name string) (bool, error) {
-	_, err := os.Stat(name)
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return err != nil, err
+	return true
 }
 
 //isBodyFile right now because of -body flag I want to check
@@ -50,22 +31,14 @@ func fileExists(name string) (bool, error) {
 //so I used a regex to test if string form body flag have special characters
 //I have to test if in not fails with something like C:\\dir1\\dir2
 func isBodyFile(s string) bool {
-
-	ret := false
+	//ret := false
 	regextTest, err := regexp.Compile("[<>\\*\\?!@$#\\(\\)\\[\\]]")
-
 	if err != nil {
 		fmt.Println("regex complie err for check is body a file: ", err)
 	}
 
 	if regextTest.MatchString(s) {
-		//fmt.Println("It's not a file (as regex result)")
-		ret = false
-	} else {
-		ret2, err := fileExists(s)
-		fmt.Println("err: ", err)
-		ret = ret2
+		return false
 	}
-
-	return ret
+	return fileExists(s)
 }
